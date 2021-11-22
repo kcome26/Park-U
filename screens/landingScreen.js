e1b0ef2af02b48d91d93fed98de5362953d7000d
Kendall Comeaux <kcome26@lsu.edu>
2021-11-22 17:13:24 -0600

    added functioning map

--- screens/landingScreen.js
+++ screens/landingScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 const HomeScreen = ({ navigation }) => {
     let location = {
@@ -13,6 +13,7 @@ const HomeScreen = ({ navigation }) => {
         height: '50%',
         position: 'absolute',
         bottom: 20,
+        borderRadius: 20,
         };
     return(
         <View style={styles.container}>
@@ -23,6 +24,13 @@ const HomeScreen = ({ navigation }) => {
                 region={location}
             >
             </MapView>
+            <View style={styles.inputs}>
+                <TouchableOpacity style={styles.button} onPress={() =>
+                    navigation.navigate('ParkingLot', { name: 'ParkingLot'})
+                    }>
+                    <Text style={styles.buttonText}>Parking Lot</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 
@@ -36,6 +44,83 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    image: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+    },  
+    card: {
+        flex: 1,
+        backgroundColor: 'rgba(255, 255, 255, 0.4)',
+        width: '80%',
+        marginTop: '40%',
+        borderRadius: 20,
+        maxHeight: 380,
+        paddingBottom: '30%',
+    },
+    heading: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginLeft: '10%',
+        marginTop: '5%',
+        marginBottom: '30%',
+        color: 'black',
+    },
+    form: {
+        flex: 1,
+        justifyContent: 'space-between',
+        paddingBottom: '5%',
+    },
+    inputs: {
+        width: '100%',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: '10%',
+    },  
+    input: {
+        width: '80%',
+        borderBottomWidth: 1,
+        borderBottomColor: 'black',
+        paddingTop: 10,
+        fontSize: 16, 
+        minHeight: 40,
+    },
+    button: {
+        width: '40%',
+        backgroundColor: 'black',
+        height: 40,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        bottom: 300,
+        left: 100,
+        marginVertical: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: '400'
+    },
+    buttonAlt: {
+        width: '80%',
+        borderWidth: 1,
+        height: 40,
+        borderRadius: 50,
+        borderColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 5,
+    },
+    buttonAltText: {
+        color: 'black',
+        fontSize: 16,
+        fontWeight: '400',
+    },
+    message: {
+        fontSize: 16,
+        marginVertical: '5%',
     },
   });
 export default HomeScreen;
